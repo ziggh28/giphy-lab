6fb02cf06647a82b01ab6fc2b5f298086cd46d40
ziggh28 <haiderzouhrab.1128@gmail.com>
2021-08-04 18:10:50 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor(props){
+
+    super(props)
+
+    this.state = {
+
+      gifName: '',
+
+      images: []
+    }
+  }
+  handleChange = (evt) => {
+    
+    this.setState({
+    
+      [evt.target.id]: evt.target.value
+    })
+  }
+  handleSubmit = (evt) => {
+    
+    evt.preventDefault()
+    
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_APIKEY}&q=${evt.target.gifName.value}`
+    
+    fetch(url).then(response => {
+    
+      return response.json()
+    
+    }).then(json => {
+    
+      this.setState({
+    
+        images: json.data
+      })
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+  render(){
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <input
+            id="gifName"
+            type="text"
+            value={this.state.gifName}
+            onChange={this.handleChange}/>
+          <button type="submit">Find this gif</button>
+        </form>
+        {
+          this.state.images.map(image => {
+            return <img src={image.images.original.url} alt="giphy gif"/>
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
